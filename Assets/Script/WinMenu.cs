632f084bd82567bdbd18f20232e77fce0fc8e697
matthew <sorawit.wann@bumail.net>
2024-10-09 06:37:25 +0700

    Add Victory Scene
    มีปุ่ม retry กับ main menu ที่พร้อมใช้งาน แต่ยังไม่ได้เชื่อมกับ scene3

---
+++ Assets/Script/WinMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour
+{
+    public string mainMenu = "MainMenu";
+
+    public void retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+    
+    
+}
